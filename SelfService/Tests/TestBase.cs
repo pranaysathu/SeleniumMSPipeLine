ac4d524521ebab47fe5ac930ad25e9bc91b01fef
sathu pranay kumar <sathupranay@gmail.com>
2019-06-25 22:02:11 +0530

    deleteTeamcity

--- SelfService/Tests/TestBase.cs
+++ SelfService/Tests/TestBase.cs
@@ -6,14 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Framework
-<<<<<<< HEAD
 {
     /// <summary>
     /// Basic Test for TestBase
     /// </summary>
-=======
-{    
->>>>>>> 4e253dfcbfa9f5e508a06f0b42b14ab46d39432a
+    
     public class TestBase
     {
         public IWebDriver driver = DriverContainer.webDriver();
