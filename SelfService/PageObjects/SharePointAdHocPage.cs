575ceaffa9e06c5b2f1742d0984805db5dc5778c
sathu pranay kumar <sathupranay@gmail.com>
2019-07-03 23:03:10 +0530

    adde POB

--- SelfService/PageObjects/SharePointAdHocPage.cs
+++ SelfService/PageObjects/SharePointAdHocPage.cs
@@ -12,10 +12,17 @@ namespace SelfService
         public void openReport()
         {
             this.Adhoc.Click();
+            this.NewButton.Click();
         }
 
         #region PageObjects
         public IWebElement Adhoc => driver.FindElement(By.XPath(""));
+
+        public IWebElement NewButton => driver.FindElement(By.XPath(""));
+
+        public IWebElement LinkInput => driver.FindElement(By.XPath(""));
+
+        public IWebElement Save => driver.FindElement(By.XPath(""));
         #endregion
     }
 }
