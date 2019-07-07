28c7a1eaee3de091880e4053f396a35ca2e6f572
sathu pranay kumar <sathupranay@gmail.com>
2019-07-07 09:27:59 +0530

    Updated driver container

--- SelfService/PageObjects/DriverContainer.cs
+++ SelfService/PageObjects/DriverContainer.cs
@@ -51,7 +51,7 @@ namespace SelfService
         //    return driver;
         //}
 
-
+        //Newly created driver
         public static IWebDriver webDriver()
         {
             driver = new ChromeDriver();
